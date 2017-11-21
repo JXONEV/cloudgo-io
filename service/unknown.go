42241a22a69f86a583a898b19076ac90310304f7
jxonev <1471829192@qq.com>
2017-11-21 21:46:59 +0800

    changes

---
+++ service/unknown.go
@@ -0,0 +1,13 @@
+package service
+
+import (
+    "net/http"
+	"fmt"
+)
+
+func unknown() http.HandlerFunc {
+
+    return func(w http.ResponseWriter, req *http.Request) {
+        fmt.Fprintf(w, "501 Not Implemented")
+    }
+}
